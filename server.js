28db3f7cc00330c94b38d789ec99307480ffbcb3
Iara Fiap <projetoiarafiap2017@gmail.com>
2017-09-09 17:36:50 +0000

    Funcional

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 router.use(bodyParser.json());
-//router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.urlencoded({ extended: true }));
 
 var jsonParser = bodyParser.json();
@@ -66,9 +65,6 @@ router.post('/webhook', jsonParser, function(req, res){
         
         console.log('INFO: Mensagem existe');
         entry.messaging.forEach(function (event){
-          //console.log('Pegou o event');
-          //console.log(event);
-          
           if(event.message){
             trataMensagem(event);
           }
@@ -133,7 +129,6 @@ function trataMensagem(event) {
 }
 
 function sendMessageFacebook(recipientId, text){
-  
   var messageData = {
         recipient: {
           id: recipientId
@@ -164,61 +159,6 @@ function sendMessageFacebook(recipientId, text){
   });  
 }
 
-/*
-io.on('connection', function (socket) {
-    messages.forEach(function (data) {
-      socket.emit('message', data);
-    });
-
-    sockets.push(socket);
-
-    socket.on('disconnect', function () {
-      sockets.splice(sockets.indexOf(socket), 1);
-      updateRoster();
-    });
-
-    socket.on('message', function (msg) {
-      var text = String(msg || '');
-
-      if (!text)
-        return;
-
-      socket.get('name', function (err, name) {
-        var data = {
-          name: name,
-          text: text
-        };
-
-        broadcast('message', data);
-        messages.push(data);
-      });
-    });
-
-    socket.on('identify', function (name) {
-      socket.set('name', String(name || 'Anonymous'), function (err) {
-        updateRoster();
-      });
-    });
-  });
-
-function updateRoster() {
-  async.map(
-    sockets,
-    function (socket, callback) {
-      socket.get('name', callback);
-    },
-    function (err, names) {
-      broadcast('roster', names);
-    }
-  );
-}
-
-function broadcast(event, data) {
-  sockets.forEach(function (socket) {
-    socket.emit(event, data);
-  });
-}
-*/
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
